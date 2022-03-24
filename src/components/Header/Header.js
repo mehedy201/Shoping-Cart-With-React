170771b0a3720cf7184d785c52e74a422e3b2135
mehedy201 <mehedy643@gmail.com>
2022-03-24 11:05:23 +0600

    added header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '../../Mehedi_Hasan.png'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className="container">
+            <nav>
+                <img src={logo} alt="" />
+                <div>
+                    <a href="/home">Home</a>
+                    <a href="/shop">Shop</a>
+                    <a href="/contact">Contact</a>
+                </div>
+            </nav>  
+        </div>
+    );
+};
+
+export default Header;
